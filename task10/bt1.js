f7ed3fb6b83c113e32c5c66c01f0ff6c8c484fb4
tien1662004 <tienndph38123@fpt.edu.vn>
2025-11-28 08:36:46 +0700

    task10

--- task10/bt1.js
+++ task10/bt1.js
@@ -1,17 +1,11 @@
 function countWords(str) {
-  // 1. Xóa khoảng trắng đầu và cuối
+
   str = str.trim();
-
-  // 2. Nếu rỗng thì trả về 0
   if (str === "") return 0;
-
-  // 3. Thay mọi dãy khoảng trắng (space, tab, newline...) thành 1 space
-  // \s+ nghĩa là "một hoặc nhiều ký tự khoảng trắng"
   str = str.replace(/\s+/g, " ");
 
-  // 4. Tách theo space và đếm phần tử
   return str.split(" ").length;
 }
 console.log(countWords("      thu aby  nom nay  acb yy "));
 console.log(countWords("   Hôm  nay   trời đẹp quá   "));
-// -> 5
+
